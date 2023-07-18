2ed25509f3cabe2d5c18148c45ae566817852650
steve zhang <871506263@qq.com>
2023-07-18 11:03:10 +0800

    提交代码于: 2023-07-18 11:03:10

--- src/views/v3/index.vue
+++ src/views/v3/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>aa</h1>
+  <div>aa</div>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive, computed, onMounted, defineProps, getCurrentInstance, nextTick, watch } from 'vue'
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+div {
+  background-color: #ccc;
+}
+</style>
