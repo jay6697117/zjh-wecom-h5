a1a8fce9bc84ce14a34bc7aef15a1b5282a9b300
steve zhang <871506263@qq.com>
2023-07-18 16:21:35 +0800

    1

--- src/views/v3/CountCompExpose/index.vue
+++ src/views/v3/CountCompExpose/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="count-comp">
-    <div style="margin-bottom: 10px;background-color: #f1f1f1;padding: 4px;width:40px;border-radius: 4px;">{{ count }}</div>
+    <h1>CountCompExpose</h1>
+    <div style="margin-bottom: 10px; background-color: #f1f1f1; padding: 4px; width: 40px; border-radius: 4px">
+      {{ count }}
+    </div>
     <van-button size="small" type="default" @click="handleAdd">Add</van-button>&nbsp;
     <van-button size="small" type="default" @click="handleSub">Sub</van-button>
   </div>
@@ -9,18 +12,24 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const count = ref(0)
+const count = ref(100)
 const handleAdd = () => {
   count.value += 1
 }
 const handleSub = () => {
   count.value -= 1
 }
+
+defineExpose({
+  count,
+  handleAdd,
+  handleSub
+})
 </script>
 
 <style lang="less" scoped>
 .count-comp {
   padding: 10px;
-  background-color: skyblue;
+  background-color: lightpink;
 }
 </style>
--- src/views/v3/index.vue
+++ src/views/v3/index.vue
@@ -1,15 +1,56 @@
 <template>
   <div class="container">
-    <van-field v-model="value1" label="文本" left-icon="smile-o" right-icon="warning-o" placeholder="显示图标" />
-    <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }"> 文本 </van-divider>
-    <count-comp />
+    <h1>111</h1>
+    <van-field
+      ref="fieldRef"
+      v-model="value1"
+      label="文本"
+      left-icon="smile-o"
+      right-icon="warning-o"
+      placeholder="显示图标"
+    />
+    <zjh-divider ref="zjhDividerRef" :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">
+      文本
+    </zjh-divider>
+    <CountCompExpose ref="countCompExposeRef" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, computed, onMounted, defineProps, getCurrentInstance, nextTick, watch } from 'vue'
+import {
+  ref,
+  reactive,
+  computed,
+  onMounted,
+  defineProps,
+  getCurrentInstance,
+  nextTick,
+  watch,
+  toRaw,
+  type ComponentInternalInstance
+} from 'vue'
+import { Divider as ZjhDivider } from 'vant'
+import CountCompExpose from './CountCompExpose/index.vue'
 
-const value1 = ref('')
+const value1 = ref('666')
+// 方法一，这个变量名和 DOM 上的 ref 属性必须同名，会自动形成绑定
+const fieldRef = ref(null)
+// 方法三，比如在ts里,可以直接获取到组件类型,可以这样获取子组件
+const countCompExposeRef = ref<InstanceType<typeof CountCompExpose> | null>(null)
+// 方法四，也可以这样 获取 vant 的组件类型
+const zjhDividerRef = ref<InstanceType<typeof ZjhDivider>>()
+
+onMounted(() => {
+  console.log('fieldRef.value', fieldRef.value)
+  console.log('countCompExposeRef', countCompExposeRef)
+  console.log('countCompExposeRef.value.count', countCompExposeRef.value?.count)
+  console.log('countCompExposeRef.value.handleAdd', countCompExposeRef.value?.handleAdd)
+  console.log('countCompExposeRef.value.handleSub', countCompExposeRef.value?.handleSub)
+  console.log('zjhDividerRef.value', zjhDividerRef.value)
+
+  const { proxy } = getCurrentInstance() as ComponentInternalInstance
+  console.log('proxy?.$refs', proxy?.$refs)
+})
 </script>
 
 <style lang="less" scoped>
