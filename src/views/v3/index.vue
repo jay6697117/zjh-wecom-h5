377ae34809498d3f9395074bae85b07151d43871
steve zhang <871506263@qq.com>
2023-07-19 15:12:03 +0800

    1

--- src/views/v3/index.vue
+++ src/views/v3/index.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="container">
     <h1>111</h1>
-    <van-field
-      ref="fieldRef"
-      v-model="value1"
-      label="文本"
-      left-icon="smile-o"
-      right-icon="warning-o"
-      placeholder="显示图标"
-    />
-    <zjh-divider ref="zjhDividerRef" :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">
-      文本
-    </zjh-divider>
-    <CountCompExpose ref="countCompExposeRef" />
   </div>
 </template>
 
@@ -21,6 +9,7 @@ import {
   ref,
   reactive,
   computed,
+  onBeforeMount,
   onMounted,
   defineProps,
   getCurrentInstance,
@@ -29,39 +18,75 @@ import {
   toRaw,
   type ComponentInternalInstance
 } from 'vue'
-import { Divider as ZjhDivider } from 'vant'
-import CountCompExpose from './CountCompExpose/index.vue'
+import { showLoadingToast } from 'vant'
 
-const value1 = ref('666')
-// 方法一，这个变量名和 DOM 上的 ref 属性必须同名，会自动形成绑定
-const fieldRef = ref(null)
-// 方法三，比如在ts里,可以直接获取到组件类型,可以这样获取子组件
-const countCompExposeRef = ref<InstanceType<typeof CountCompExpose> | null>(null)
-// 方法四，也可以这样 获取 vant 的组件类型
-const zjhDividerRef = ref<InstanceType<typeof ZjhDivider>>()
+const p1 = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ msg: '请求成功', data: [1, 2, 3], code: 200 })
+    }, 2000)
+  })
+}
 
+const getData = async () => {
+  const toastInst = showLoadingToast({
+    duration: 0,
+    forbidClick: false,
+    message: '加载中',
+    loadingType: 'spinner',
+    className: 'loading-toast',
+    teleport: document.getElementsByClassName('container')[0]
+  })
+  try {
+    const res = await p1()
+    console.log('res',Date.now(), res)
+    toastInst.close()
+  } catch (error) {
+    toastInst.close()
+    console.log('error', error)
+  }
+}
+
+onBeforeMount(() => {
+  console.log('onBeforeMount run');
+  getData()
+})
 onMounted(() => {
-  console.log('fieldRef.value', fieldRef.value)
-  console.log('countCompExposeRef', countCompExposeRef)
-  console.log('countCompExposeRef.value.count', countCompExposeRef.value?.count)
-  console.log('countCompExposeRef.value.handleAdd', countCompExposeRef.value?.handleAdd)
-  console.log('countCompExposeRef.value.handleSub', countCompExposeRef.value?.handleSub)
-  console.log('zjhDividerRef.value', zjhDividerRef.value)
-
-  const { proxy } = getCurrentInstance() as ComponentInternalInstance
-  console.log('proxy?.$refs', proxy?.$refs)
+  console.log('onMounted run');
+  getData()
 })
 </script>
+
+
+<style lang="less">
+.loading-toast.van-popup {
+  box-sizing: border-box;
+  max-width: 375px;
+  max-height: 375px;
+  width: 335px;
+  height: 335px;
+}
+
+.loading-toast {
+  .van-loading__spinner {
+    color: red;
+    width: 60px;
+    height: 60px;
+  }
+
+  .van-toast__text {
+    color: red;
+    font-size: 80px;
+    height: 100px;
+    line-height: 100px;
+  }
+}
+</style>
 
 <style lang="less" scoped>
 .container {
   width: 375px;
-  height: 100%;
   padding: 15px 10px;
-  background-color: #f8f8f8;
-
-  .content {
-    background-color: skyblue;
-  }
+  background-color: #ccc;
 }
 </style>
