92298325e7891d725cd5d787c4da268977c41af8
steve zhang <871506263@qq.com>
2023-07-29 17:22:29 +0800

    1

--- src/views/v3/index.vue
+++ src/views/v3/index.vue
@@ -1,22 +1,11 @@
-<script setup>
-import { ref } from 'vue'
-const str = ref('blue') // 蓝色
-// let str = 'blue'
-setTimeout(() => {
-  // str = 'purple' // 非响应式数据不可以动态改变
-  str.value = 'purple' // 响应式数据可以动态改变
-}, 2000)
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
+import vl from './vl/index.vue'
+
 </script>
 
 <template>
-  <div class="name">沐华</div>
+  <vl />
 </template>
 
-<style lang="less" scoped>
-.name {
-  font-size: 30px;
-  color: red;
-  padding: 20px;
-  background-color: v-bind(str); // JS 中的色值变量 #f00 就赋值到这来了
-}
-</style>
+<style lang="less" scoped></style>
